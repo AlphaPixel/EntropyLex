0b1f36f8faeb973aaae484d1d99a03bd46f707ff
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2026-08-17 17:45:34 -0600

    Issue #5: Create scaffolding for go/arcadium (#6)
    * Issue #5: Create scaffolding for go/arcadium
    
    * rename task, fix make commands
    
    * add working-directory to go-arcadium-test
    
    * remove the fortran .gitignore, add go.mod

---
+++ go/arcadium/entropylex/entropylex_test.go
@@ -0,0 +1,15 @@
+package entropylex8_test
+
+import (
+	"testing"
+)
+
+func Test_NewEncoder(t *testing.T) {
+	t.Parallel()
+	// TODO
+}
+
+func Test_NewDecoder(t *testing.T) {
+	t.Parallel()
+	// TODO
+}
---
+++ go/arcadium/entropylex8/encoding_test.go
@@ -0,0 +1,10 @@
+package entropylex8_test
+
+import (
+	"testing"
+)
+
+func Test_NewEncoding(t *testing.T) {
+	t.Parallel()
+	// TODO
+}
