3b24886e9e67456727977edcc7dce3083ca2e3b5
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2026-08-18 18:51:47 -0600

    Issue #16: Create Go LXJ dictionary implementation (#17)
    * Issue #16: Create Go LXJ dictionary implementation
    
    * update go.mod to pull in latest golang.org/x/text
    
    * adjust the go-arcardium-test actions to match the new go src directory

---
+++ src/go/ian-cahoon/assert/assert.go
@@ -0,0 +1,144 @@
+// Copyright 2021-2026 arcadium.dev <info@arcadium.dev>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package assert provides a set of tools for use with the Go testing package.
+package assert
+
+import (
+	"errors"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// Contains asserts that the expected string is contained in the actual string.
+func Contains(t *testing.T, actual, expected string) {
+	t.Helper()
+	if !strings.Contains(actual, expected) {
+		t.Errorf("\nActual:   %s\nExpected: %s", actual, expected)
+	}
+}
+
+// Equal asserts that the expected value is equal to the actual value.
+// Actual and expected must be comparable. To compare non-comparable
+// types use Compare.
+func Equal[T comparable](t *testing.T, actual, expected T) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("\nActual:   %+v\nExpected: %+v", actual, expected)
+	}
+}
+
+// NotEqual asserts that the expected value is not equal to the actual value.
+// Actual and expected must be comparable. To compare non-comparable types use
+// Compare.
+func NotEqual[T comparable](t *testing.T, actual, expected T) {
+	t.Helper()
+	if actual == expected {
+		t.Errorf("Expected different values: %+v", actual)
+	}
+}
+
+// Compare asserts that the expected value is equal to the actual value.
+// Actual and expected need not be comparable.
+func Compare[T any](t *testing.T, actual, expected T, opts ...cmp.Option) {
+	t.Helper()
+	if !cmp.Equal(actual, expected, opts...) {
+		t.Errorf("\nActual:   %+v\nExpected: %+v", actual, expected)
+	}
+}
+
+// Error asserts that the error string from err matches the expected.
+func Error(t *testing.T, err error, expected string) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("Expected an error")
+	}
+	if expected != err.Error() {
+		t.Errorf("\nActual error:   %s\nExpected error: %s", err, expected)
+	}
+}
+
+// IsError asserts that the actual error matches the expected error.
+func IsError(t *testing.T, actual, expected error) {
+	t.Helper()
+	if actual == nil {
+		t.Fatal("Expected an error")
+	}
+	if !errors.Is(actual, expected) {
+		t.Errorf("\nActual:   %s\nExpected: %s", actual, expected)
+	}
+}
+
+// Nil asserts that the value of the given object is nil.
+func Nil(t *testing.T, object any) {
+	t.Helper()
+	if object == nil {
+		return
+	}
+
+	value := reflect.ValueOf(object)
+	switch value.Kind() {
+	case
+		reflect.Chan, reflect.Func,
+		reflect.Interface, reflect.Map,
+		reflect.Pointer, reflect.Slice, reflect.UnsafePointer:
+
+		if value.IsNil() {
+			return
+		}
+	}
+
+	t.Errorf("Unexpected non-nil value: %+v", object)
+}
+
+// NotNil asserts that the value of the given object is not nil.
+func NotNil(t *testing.T, object any) {
+	t.Helper()
+	if object != nil {
+		return
+	}
+
+	value := reflect.ValueOf(object)
+	switch value.Kind() {
+	case
+		reflect.Chan, reflect.Func,
+		reflect.Interface, reflect.Map,
+		reflect.Pointer, reflect.Slice, reflect.UnsafePointer:
+
+		if !value.IsNil() {
+			return
+		}
+	}
+
+	t.Errorf("Unexpected nil value: %+v", object)
+}
+
+// True asserts that the given value is true.
+func True(t *testing.T, value bool) {
+	t.Helper()
+	if !value {
+		t.Error("Expected value to be true")
+	}
+}
+
+// False asserts that the given value is false.
+func False(t *testing.T, value bool) {
+	t.Helper()
+	if value {
+		t.Error("Expected value to be false")
+	}
+}
---
+++ src/go/ian-cahoon/dictionary/lxj_test.go
@@ -0,0 +1,105 @@
+package dictionary_test
+
+import (
+	"testing"
+
+	"github.com/AlphaPixel/EntropyLex/src/go/ian-cahoon/assert"
+	"github.com/AlphaPixel/EntropyLex/src/go/ian-cahoon/dictionary"
+	"github.com/AlphaPixel/EntropyLex/src/go/ian-cahoon/require"
+)
+
+func Test_NewLXJ(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		filename string
+		verify   func(*testing.T, *dictionary.Dictionary, error)
+	}{
+		{
+			name:     "empty filename",
+			filename: "",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, d)
+				assert.Error(t, err, "file name required")
+			},
+		},
+		{
+			name:     "file doesn't exist",
+			filename: "file-does-not-exist.json",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, d)
+				assert.Error(t, err, "open file-does-not-exist.json: no such file or directory")
+			},
+		},
+		{
+			name:     "valid dictionary",
+			filename: "el-8-valid.lxj",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, err)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			d, err := dictionary.NewLXJ(test.filename)
+			test.verify(t, d, err)
+		})
+	}
+}
+
+func Test_NewXJValidated(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		filename string
+		verify   func(*testing.T, *dictionary.Dictionary, error)
+	}{
+		{
+			name:     "empty filename",
+			filename: "",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, d)
+				assert.Error(t, err, "file name required")
+			},
+		},
+		{
+			name:     "file doesn't exist",
+			filename: "file-does-not-exist.json",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, d)
+				assert.Error(t, err, "open file-does-not-exist.json: no such file or directory")
+			},
+		},
+		{
+			name:     "invalid dictionary",
+			filename: "el-8-invalid.lxj",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, d)
+				require.NotNil(t, err)
+				assert.Contains(t, err.Error(), "jsonschema validation failed with")
+				assert.Contains(t, err.Error(), "at '/format/name': value must be 'LXJ")
+			},
+		},
+		{
+			name:     "valid dictionary",
+			filename: "el-8-valid.lxj",
+			verify: func(t *testing.T, d *dictionary.Dictionary, err error) {
+				assert.Nil(t, err)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			d, err := dictionary.NewLXJValidated(test.filename)
+			test.verify(t, d, err)
+		})
+	}
+}
