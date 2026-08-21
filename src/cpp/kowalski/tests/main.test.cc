2afc7ef20cb9edd2cf2e1c5f8845159f11a51d55
Max Morgan <maxwelltmorgan@pm.me>
2026-08-20 20:06:49 -0600

    feat: removed placeholder test

--- src/cpp/kowalski/tests/main.test.cc
+++ src/cpp/kowalski/tests/main.test.cc
@@ -4,14 +4,6 @@
 #include "util.h"
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
-
 TEST(SchemaTest, FileOpensAsJson) {
   auto path = fixture_path("schema/el-8-valid.lxj");
   std::ifstream file(path);
